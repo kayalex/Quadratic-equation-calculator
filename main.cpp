5966b1346767d6a858bf17d6adbccb646e9f1072
kayalex <kchaminiski123@outlook.com>
2022-07-29 14:18:33 +0200

    final commit after making quadratic equation calculator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-
-int product(int num1, int num2){
-    return num1 * num2;
-}
-
+#include <cmath>
 
 int main(){
-    int firstNumber {3};
-    float secondNumber {7};
-    std::cout << product(firstNumber, secondNumber);
+    int a;
+    int b;
+    int c;
+    double x1;
+    double x2;
+    // enter value for a
+    std::cout <<"Enter the value of a: ";
+    std::cin >> a;
+
+    // enter value for b
+    std::cout <<"Enter the value of b: ";
+    std::cin >> b;
+    // enter value for c
+    std::cout <<"Enter the value of c: ";
+    std::cin >> c;
+    float root = std::sqrt(((b*b)-(4*a*c)));
+
+    x1 = (-b + root)/(2*a);
+    x2 = (-b - root)/(2*a);
+
+    std::cout << "Either x = " << x1 << std::endl;
+    std::cout << "or x = " << x2 << std::endl;
+    
     return 0;
 }
